e4cb9034a5895563f4a8228884de77683a681ed3
Darren Hardy <drh@stanford.edu>
2014-04-17 16:01:22 -0700

    solr upload

---
+++ test/upload.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+c=kurma-app-dev
+d=geoblacklight
+h=localhost:8983
+s=http
+
+find -L "$d" -name 'stanford*.xml' | while read fn; do
+  echo "Uploading $fn"
+  curl  -X POST \
+        -H 'Content-Type: text/xml' \
+        --data-binary "@$fn" \
+        "$s://${h}/solr/${c}/update"
+  curl "$s://${h}/solr/${c}/update?commit=true"
+done
+curl "$s://${h}/solr/${c}/update?optimize=true"
+
