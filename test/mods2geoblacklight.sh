1f0cf8d096aa589461b2991c06be34dd11ec7a05
Darren Hardy <drh@stanford.edu>
2014-04-30 14:37:44 -0700

    added now parameter

--- test/mods2geoblacklight.sh
+++ test/mods2geoblacklight.sh
@@ -6,6 +6,7 @@ find mods -name 'stanford*.xml' -print | while read fn; do
   if [ ! -r "$ofn" ]; then
     xsltproc \
       -stringparam geoserver_root 'http://kurma-podd1.stanford.edu/geoserver' \
+      -stringparam now `date -u "+%Y-%m-%dT%H:%M:00Z"` \
 			../lib/xslt/mods2geoblacklight.xsl "$fn" > "$ofn"
     # -stringparam purl 'http://purl.stanford.edu/aa111bb2222' \
   fi
