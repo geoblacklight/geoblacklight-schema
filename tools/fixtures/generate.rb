ae9e2e354c5da3cbfe1e7bfb0482ef9238e79845
Darren Hardy <drh@stanford.edu>
2014-12-05 11:34:02 -0800

    added 14 cases and renumbered them

--- tools/fixtures/generate.rb
+++ tools/fixtures/generate.rb
@@ -18,15 +18,15 @@ uri = {
       wms: 'http://www.opengis.net/def/serviceType/ogc/wms'
     }
 
-def generate(n, *params)
-  name = "case#{n}"
+def generate(i, *params)
+  name = "case#{i}"
   puts "Generating #{name}"
   s, w, n, e = [-5, -30, 15, 45]
   layer = Hash(*params)
   layer.merge!({
     :uuid => "http://purl.stanford.edu/#{name}",
     :dc_identifier_s => "http://purl.stanford.edu/#{name}",
-    :dc_description_s => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    :dc_description_s => "Lorem ipsum dolor sit amet...",
     :dc_rights_s => 'Public',
     :dct_provenance_s => 'Stanford',
     :georss_box_s => "#{s} #{w} #{n} #{e}",
@@ -35,7 +35,7 @@ def generate(n, *params)
     :solr_bbox  => "#{w} #{s} #{e} #{n}", # minX minY maxX maxY
     :solr_geom  => "ENVELOPE(#{w}, #{e}, #{n}, #{s})"    
   })
-  layer[:dc_title_s] = "Case #{n}: #{layer[:dc_title_s]}"
+  layer[:dc_title_s] = "Case #{i}: #{layer[:dc_title_s]}"
   layer[:dct_references_s] = '{}' if layer[:dct_references_s].nil?
   
   json = JSON.pretty_generate(layer)
@@ -45,21 +45,21 @@ end
 
 generate(1,
   dc_title_s:         'Bare requirements',
-  dc_format_s: 'Paper',
+  dc_format_s:        'Paper',
   layer_geom_type_s:  'Paper Map'
 )
 
 generate(2,
   dc_title_s:         'Bare requirements with URL',
-  dc_format_s: 'Paper',
+  dc_format_s:        'Paper',
   layer_geom_type_s:  'Paper Map',
   dct_references_s:   {
-    uri[:url] => "http://localhost/data",
+    uri[:url] => "http://localhost/homepage",
   }
 )
 
-generate(10,
-  dc_title_s:         'Scanned Map',
+generate(3,
+  dc_title_s:         'Scanned Map with download',
   dc_format_s:        'TIFF',
   layer_geom_type_s:  'Scanned Map',
   dct_references_s:   {
@@ -67,59 +67,118 @@ generate(10,
   }
 )
 
-generate(11,
-  dc_title_s:         'Scanned Map with Thumbnail',
+generate(4,
+  dc_title_s:         'Scanned Map with download and thumbnail',
   dc_format_s:        'TIFF',
   layer_geom_type_s:  'Scanned Map',
   dct_references_s:   {
+    uri[:download] => "http://localhost/data.zip",
     uri[:thumbnail] => "http://localhost/data.jpg"
   }
 )
 
-generate(12,
+generate(5,
   dc_title_s:         'Scanned Map with IIIF',
   dc_format_s:        'TIFF',
   layer_geom_type_s:  'Scanned Map',
   dct_references_s:   {
-    uri[:iiif] => "http://localhost:8080/iiif"
+    uri[:iiif] => "http://localhost/iiif"
   }
 )
 
-generate(20,
+generate(6,
   dc_title_s:         'Raster with WMS only',
   dc_format_s:        'GeoTIFF',
   layer_geom_type_s:  'Raster',
   dct_references_s:   {
-    uri[:wms] => "http://localhost:8080/wms"
+    uri[:wms] => "http://localhost/wms"
   }
 )
 
-generate(21,
+generate(7,
   dc_title_s:         'Shapefile with WMS only',
   dc_format_s:        'Shapefile',
   layer_geom_type_s:  'Polygon',
   dct_references_s:   {
-    uri[:wms] => "http://localhost:8080/wms"
+    uri[:wms] => "http://localhost/wms"
   }
 )
 
-generate(22,
+generate(8,
   dc_title_s:         'Shapefile with WMS and download',
   dc_format_s:        'Shapefile',
   layer_geom_type_s:  'Polygon',
   dct_references_s:   {
     uri[:download] => "http://localhost/data.zip",
-    uri[:wms] => "http://localhost:8080/wms"
+    uri[:wms] => "http://localhost/wms"
   }
 )
 
-generate(23,
+generate(9,
   dc_title_s:         'Shapefile with WMS and download and URL',
   dc_format_s:        'Shapefile',
   layer_geom_type_s:  'Polygon',
   dct_references_s:   {
     uri[:url] => "http://localhost/homepage",
     uri[:download] => "http://localhost/data.zip",
-    uri[:wms] => "http://localhost:8080/wms"
+    uri[:wms] => "http://localhost/wms"
+  }
+)
+
+generate(10,
+  dc_title_s:         'Shapefile with WMS/WFS and download and URL',
+  dc_format_s:        'Shapefile',
+  layer_geom_type_s:  'Polygon',
+  dct_references_s:   {
+    uri[:url] => "http://localhost/homepage",
+    uri[:download] => "http://localhost/data.zip",
+    uri[:wms] => "http://localhost/wms",
+    uri[:wfs] => "http://localhost/wfs"
+  }
+)
+
+generate(11,
+  dc_title_s:         'Raster with WMS/WCS and download and URL',
+  dc_format_s:        'GeoTIFF',
+  layer_geom_type_s:  'Raster',
+  dct_references_s:   {
+    uri[:url] => "http://localhost/homepage",
+    uri[:download] => "http://localhost/data.zip",
+    uri[:wms] => "http://localhost/wms",
+    uri[:wcs] => "http://localhost/wcs"
+  }
+)
+
+generate(12,
+  dc_title_s:         'Bare requirements with metadata',
+  dc_format_s:        'Paper',
+  layer_geom_type_s:  'Paper Map',
+  dct_references_s:   {
+    uri[:fgdc] => "http://localhost/fgdc.xml"
+  }
+)
+
+generate(13,
+  dc_title_s:         'Bare requirements with multiple metadata formats',
+  dc_format_s:        'Paper',
+  layer_geom_type_s:  'Paper Map',
+  dct_references_s:   {
+    uri[:fgdc] => "http://localhost/fgdc.xml",
+    uri[:iso19139] => "http://localhost/iso19139.xml",
+    uri[:html] => "http://localhost/fgdc.html"
+  }
+)
+
+generate(14,
+  dc_title_s:         'Raster with WMS/WCS and download and URL and metadata',
+  dc_format_s:        'GeoTIFF',
+  layer_geom_type_s:  'Raster',
+  dct_references_s:   {
+    uri[:url] => "http://localhost/homepage",
+    uri[:download] => "http://localhost/data.zip",
+    uri[:wms] => "http://localhost/wms",
+    uri[:wcs] => "http://localhost/wcs",
+    uri[:iso19139] => "http://localhost/iso19139.xml",
+    uri[:html] => "http://localhost/iso19139.html"
   }
 )
