7c51eb674a6dd935d4d7b4aef6f9de485e022a7e
Darren Hardy <drh@stanford.edu>
2014-12-15 13:16:26 -0800

    migrate to Mikes fgdc2html code and CSS

--- tools/fgdc2html/render.rb
+++ tools/fgdc2html/render.rb
@@ -5,13 +5,17 @@
 require 'nokogiri'
 
 class RenderCLI
-  @@xslt = Nokogiri::XSLT(File.read(File.join(File.dirname(__FILE__), 'fgdc-html.xsl')))
+  @@xslt = Nokogiri::XSLT(File.read(File.join(File.dirname(__FILE__), 'fgdc2html.xsl')))
   
   def run(xmlfn)
     doc = Nokogiri::XML(File.read(xmlfn))
     htmlfn = xmlfn.gsub(/\.xml$/, '.html')
     File.open(htmlfn, 'wb') do |f|
+      f << '<html><head><style>'
+      f << File.read(File.join(File.dirname(__FILE__), 'fgdc2html.css'))
+      f << '</style></head><body>'
       f << @@xslt.transform(doc)
+      f << '</body></html>'
     end
   end
 end
