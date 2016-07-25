5d35eda5b053c76619300f30b9c7f785121dc883
Darren Hardy <drh@stanford.edu>
2016-07-25 14:47:09 -0700

    switch to json-schema gem

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'json'
+gem 'json-schema'
 gem 'prmd'
 gem 'rsolr'
--- tools/validation/validate.rb
+++ tools/validation/validate.rb
@@ -1,21 +1,23 @@
 #!/usr/bin/env ruby
 
 require 'json'
-require 'json_schema'
+require 'json-schema'
+
+$verbose = false
 
 # validate some data - raise ValidationError if it doesn't conform
 def validate(schema, v)
-  schema.validate!(v)
-rescue JsonSchema::ValidationError, RuntimeError => e
+  JSON::Validator.validate!(schema, v)
+  puts "UUID: #{v['uuid']}: OK" if $verbose
+rescue JSON::Schema::ValidationError => e
   puts "UUID: #{v['uuid']}: #{e}"
 end
 
 # parse the schema - raise SchemaError if it's invalid
-schema_data = JSON.parse(File.read('../../geoblacklight-schema.json'))
-schema = JsonSchema.parse!(schema_data)
+schema = JSON.parse(File.read('../../geoblacklight-schema.json'))
 
 ARGV.each do |fn|
-  puts "Validating #{fn}"
+  puts "Validating #{fn}" if $verbose
   data = JSON.parse(File.read(fn))
   if data.is_a?(Array)
     data.each do |v|
